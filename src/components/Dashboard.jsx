d9d0df4537f1ee64821240ebd464c47602e04d1b
ArtyomAmirdzhanyans <tyoma_manager@mail.ru>
2022-12-13 16:15:47 +0300

    Creating a sidebar and a contact modal

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import Sidebar from "./Sidebar";
 
 const Dashboard = ({ id }) => {
-  return <div>{id}</div>;
+  return (
+    <div className="d-flex" style={{ height: "100vh" }}>
+      <Sidebar id={id} />
+    </div>
+  );
 };
 
 export default Dashboard;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import { Tab, Nav, Button, Modal } from "react-bootstrap";
+
+import Contacts from "./Contacts";
+import Conversations from "./Conversations";
+import NewContactModal from "./NewContactModal";
+import NewConversationModal from "./NewConversationModal";
+
+const CONVERSATIONS_KEY = "conversations";
+const CONTACTS_KEY = "contacts";
+
+const Sidebar = ({ id }) => {
+  const [activeKey, setActiveKey] = useState(CONVERSATIONS_KEY);
+  ///! The default tab is not working for the moment, need to fix it later
+  const [modalOpen, setModalOpen] = useState(false);
+  const conversationOpen = activeKey === CONVERSATIONS_KEY;
+
+  const closeModal = () => {
+    setModalOpen(false);
+  };
+
+  return (
+    <div style={{ width: "250px" }} className="d-flex flex-column">
+      <Tab.Container active={activeKey} onSelect={setActiveKey}>
+        <Nav variant="tabs" className="justify-content-center">
+          <Nav.Item>
+            <Nav.Link eventKey={CONVERSATIONS_KEY}>Conversations</Nav.Link>
+          </Nav.Item>
+          <Nav.Item>
+            <Nav.Link eventKey={CONTACTS_KEY}>Contacts</Nav.Link>
+          </Nav.Item>
+        </Nav>
+        <Tab.Content className="border-right overflow-auto flex-grow-1">
+          <Tab.Pane eventKey={CONVERSATIONS_KEY}>
+            <Conversations />
+          </Tab.Pane>
+          <Tab.Pane eventKey={CONTACTS_KEY}>
+            <Contacts />
+          </Tab.Pane>
+        </Tab.Content>
+        <div className="p-2 border-top border-right small">
+          Your Id: <span className="text-muted">{id}</span>
+        </div>
+        <Button className="rounded-0" onClick={() => setModalOpen(true)}>
+          New {conversationOpen ? "Conversation" : "Contact"}
+        </Button>
+      </Tab.Container>
+      <Modal show={modalOpen} onHide={closeModal}>
+        {conversationOpen ? (
+          <NewConversationModal closeModal={closeModal} />
+        ) : (
+          <NewContactModal closeModal={closeModal} />
+        )}
+      </Modal>
+    </div>
+  );
+};
+
+export default Sidebar;
